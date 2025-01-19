b98a058808f775d4f6ac3cc884cbb1dedecaaae3
Momin Ali <80389861+Momin9@users.noreply.github.com>
2025-01-20 02:02:15 +0500

    Delete threat_detection directory

--- threat_detection/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ThreatDetectionAPI
-
-urlpatterns = [
-    path('api/threat-detection/', ThreatDetectionAPI.as_view(), name='threat_detection_api'),
-]
